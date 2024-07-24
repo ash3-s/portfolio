7c6c70ba5bcb95beb3c3502485ec8010b338115b
ash3-s <107628402+ash3-s@users.noreply.github.com>
2024-07-25 00:47:46 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -75,7 +75,7 @@ const Index = () => {
               </a>
               <a
                 href="#"
-                className="block px-3 py-2 rounded-md text-base font-medium text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-900"
+                className="block px-3 py-2 rounded-md text-base font-medium text-gray-300 hover:text-white hover:bg-gray-800 focus:outline-none focus:text-white focus:bg-gray-900"
               >
                 Contact
               </a>
